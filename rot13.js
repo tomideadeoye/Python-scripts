27633b0afeb76f285229d351095fffca34800baf
Tomide Adeoye <tommideadeoye@gmail.com>
2022-12-03 18:57:41 +0100

    rot13

---
+++ rot13.js
@@ -0,0 +1,9 @@
+function rot13(message) {
+	return message.replace(/[a-z]/gi, (letter) =>
+		String.fromCharCode(
+			letter.charCodeAt(0) + (letter.toLowerCase() <= "m" ? 13 : -13)
+		)
+	);
+}
+
+console.log("I love babies") // V ybir onovrf
